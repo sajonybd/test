b98940d0d1b6f312d486c90f601692b5ea03a239
Safiul Alom <sajonybd@gmail.com>
2023-04-13 11:54:57 +0600

    v2 fixed

--- index.js
+++ index.js
@@ -34,24 +34,9 @@ app.post('/v2', (req, res) => {
   let header = headers ? JSON.stringify(headers) : '{"X-Powered-By": "Cloudflare"}';
   let proxy = data.proxy ? decodeURIComponent(data.proxy) : "";
   let method = data.method ? data.method.toUpperCase() : "GET";
+  console.log(url);
+  console.log(JSON.stringify(data.data));
   scrapeMaster(res,url,ua,header,proxy,cookie,method,data.data);
-})
-
-
-app.get("/v1", (req, res) => {
-  res.send("Scrape Master v1 Running!");
-});
-
-app.post('/v1', (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  let data = req.body;
-  let url = data.url ? decodeURI(data.url) : "https://example.com";
-  let ua = data.browser ? decodeURIComponent(data.browser) : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36";
-  let proxy = data.proxy ? decodeURIComponent(data.proxy) : "";
-  let method = data.method ? data.method.toUpperCase() : "GET";
-  let header = data.headers ? JSON.stringify(data.headers) : '["X-Powered-By: Cloudflare"]';
-  let cookie = data.cookie ? data.cookie : '';
-  scrapeLogic(res,url,ua,header,proxy,cookie,method,data.data);
 })
 
 app.listen(PORT, () => {
